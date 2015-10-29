2a1d3d98b40207f0d51f4e0bb7fc01f3a04a3495
beardog <beardog@yandex.ru>
2015-10-29 10:17:54 +0200

    Reals onsite path

--- gatheringData/gatheringSettings.sh
+++ gatheringData/gatheringSettings.sh
@@ -5,14 +5,16 @@
 
 case $USER in
   "houser" )
-    basicDataPath="/home/houser/kc/"
-    nvmPath="/home/houser/.nvm/"
+    basicDataPath="/home/houser/allcourts/gdata"
+    archiveDataPath="/home/houser/allcourts/archive"
+    recentDataPath="/home/houser/allcourts/recent"
+    nvmPath="/home/houser/.nvm"
     ;;
 
   "u1user" )
     basicDataPath="/home/u1user/proj/allcourts/gatheringData"
-    archiveDataPath="/home/u1user/proj/allcourts/devtesttmp/archive"
-    recentDataPath="/home/u1user/proj/allcourts/devtesttmp/recent"
+    archiveDataPath="/home/u1user/proj/allcourts/gatheringData/archive"
+    recentDataPath="/home/u1user/proj/allcourts/gatheringData/recent"
     nvmPath="$HOME/nodejs/nvm/"
     ;;
 
